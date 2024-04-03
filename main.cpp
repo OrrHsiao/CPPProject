43b61b00a69ad3b2307b4bac874187f49194027c
xiaoao <xiaoao@imilab.com>
2024-04-03 17:30:30 +0800

    修改代码

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void variableTest();
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    variableTest();
+    return 0;
+}
+
+void variableTest() {
+    int a = 1;
+    char b = '2';
+    printf("123");
+}
